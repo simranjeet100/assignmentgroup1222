21330fa5980553105c9cce3d13ae8599de81f17e
visualstudio <visualstudio@122-PC18>
2019-02-22 19:14:49 -0500

    commit 2

--- assignmentgroup/Program.cs
+++ assignmentgroup/Program.cs
@@ -26,3 +26,22 @@ class JournalEntry
     public int getDistanceWalked() { return distanceTraveled; }
     public string getVillageName() { return villageName; }
 }
+class Hugi
+{
+    private static JournalEntry je;
+    public static bool FoundAstrilde = false;
+
+    public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+
+    public static int CalculateDistanceWalked()
+    {
+        int DistanceWalked = 0;
+        // go down the list and add the distances
+        foreach (var je in HugiJournal)
+        {
+            Console.WriteLine(" {0}  --   {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+            DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
+        }
+        return DistanceWalked;
+    }
+}
